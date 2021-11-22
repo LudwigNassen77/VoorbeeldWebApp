0c209771013e8d3b072a987b1787616dd7075dd1
ludwignassen77 <ludwig.nassen@hasp-o.be>
2021-11-22 08:45:05 +0100

    Eerste upload van de webapplicatie

---
+++ VoorbeeldGitHub/VoorbeeldGitHub/Models/Gebruiker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VoorbeeldGitHub.Models
+{
+    public class Gebruiker
+    {
+        public string Naam { get; set; }
+        public void Start()
+        {
+            Naam = "Ludwig";
+        }
+    }
+}
